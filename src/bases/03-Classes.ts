5e5335649ca5970bcd02764692c2808f200ab067
Alejandro Javier Bernal <bernal.a.javier@gmail.com>
2023-10-24 00:08:32 -0300

    avances #1

--- src/bases/03-Classes.ts
+++ src/bases/03-Classes.ts
@@ -1,4 +1,4 @@
-
+import axios from "axios";
 
 export class Pokemons {
     //public readonly id: number;
@@ -18,16 +18,24 @@ export class Pokemons {
     
     speak() {
         console.log(`${this.name},${this.name}`);}
-    }
     
+    async getMoves() {
+        const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon/4');
+        console.log(data.moves);
+
+        return data.moves;
+    }
+}
 
 export const charmander = new Pokemons(4, 'Charmander')
 //,'https://pokemon.com/4.jpg'
 
 //charmander.id = 10;
 //charmander.name = 'Mew'
-console.log(charmander);
-charmander.scream()
-charmander.speak() 
+//console.log(charmander);
+//charmander.scream()
+//charmander.speak() 
+
+console.log( charmander.getMoves() );
 
 
